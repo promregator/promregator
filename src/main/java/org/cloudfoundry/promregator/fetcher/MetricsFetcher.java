f1677d067b0ed3cd21e258dcd15a5a4dcc819dd0
Nico Schmoigl <github@schmoigl-online.de>
2018-03-04 10:13:57 +0100

    Fixing obsolete import statements

--- src/main/java/org/cloudfoundry/promregator/fetcher/MetricsFetcher.java
+++ src/main/java/org/cloudfoundry/promregator/fetcher/MetricsFetcher.java
@@ -2,7 +2,6 @@ package org.cloudfoundry.promregator.fetcher;
 
 import java.io.IOException;
 import java.util.HashMap;
-import java.util.List;
 import java.util.concurrent.Callable;
 
 import org.apache.http.HttpHost;
