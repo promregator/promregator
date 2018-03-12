7a428e935590e21812ab9aef03113c3714117956
Nico Schmoigl <github@schmoigl-online.de>
2018-03-12 11:24:26 +0100

    Specify default location of application/promregator yaml

--- docker/data/promregator.sh
+++ docker/data/promregator.sh
@@ -10,5 +10,9 @@ if [ "$JAVA_OPTS" == "" ]; then
 	JAVA_OPTS=""
 fi
 
-$JAVACMD $JAVA_MEM_OPTS $JAVA_OPTS -jar /opt/promregator/promregator.jar
+if [ "$PROMREGATOR_CONFIG_FILE" == "" ]; then
+	PROMREGATOR_CONFIG_FILE="/etc/promregator/promregator.yml"
+fi
+
+$JAVACMD $JAVA_MEM_OPTS $JAVA_OPTS "-Dspring.config.location=$PROMREGATOR_CONFIG_FILE" -jar /opt/promregator/promregator.jar
 
