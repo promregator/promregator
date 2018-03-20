11464b4f02d755ec0828fbae47a50815282c01a3
Nico Schmoigl <github@schmoigl-online.de>
2018-03-20 22:29:24 +0100

    Fix broken unit test execution

--- src/test/java/org/cloudfoundry/promregator/PromregatorApplicationTest.java
+++ src/test/java/org/cloudfoundry/promregator/PromregatorApplicationTest.java
@@ -1,13 +1,25 @@
 package org.cloudfoundry.promregator;
 
+import org.cloudfoundry.promregator.endpoint.TestableMetricsEndpoint;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.autoconfigure.AutoConfigurationExcludeFilter;
+import org.springframework.boot.context.TypeExcludeFilter;
+import org.springframework.boot.test.context.SpringBootTestContextBootstrapper;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScan.Filter;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.test.context.BootstrapWith;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@BootstrapWith(value=SpringBootTestContextBootstrapper.class)
+@ComponentScan(excludeFilters = { @Filter(type = FilterType.CUSTOM, classes = { TypeExcludeFilter.class }),
+		@Filter(type = FilterType.CUSTOM, classes = { AutoConfigurationExcludeFilter.class }),
+		@Filter(type = FilterType.ASSIGNABLE_TYPE, classes = {  TestableMetricsEndpoint.class })
+		// NB: TestableMetricsEndpoint would break here everything
+})
 @TestPropertySource(locations="default.properties")
 public class PromregatorApplicationTest {
 
