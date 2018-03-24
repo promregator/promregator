9db352bdb242fdd789754c2f4242762d49cbf94c
Nico Schmoigl <github@schmoigl-online.de>
2018-03-24 10:39:07 +0100

    Refactoring: Use abstract class patthern to centralize scraping logic

--- src/main/java/org/cloudfoundry/promregator/endpoint/MetricsEndpoint.java
+++ src/main/java/org/cloudfoundry/promregator/endpoint/MetricsEndpoint.java
@@ -1,203 +1,37 @@
 package org.cloudfoundry.promregator.endpoint;
 
-import java.time.Duration;
-import java.time.Instant;
-import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Future;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
 
-import javax.annotation.PostConstruct;
-
-import org.apache.log4j.Logger;
-import org.cloudfoundry.promregator.auth.AuthenticationEnricher;
-import org.cloudfoundry.promregator.config.PromregatorConfiguration;
-import org.cloudfoundry.promregator.config.Target;
-import org.cloudfoundry.promregator.fetcher.CFMetricsFetcher;
-import org.cloudfoundry.promregator.fetcher.MetricsFetcher;
-import org.cloudfoundry.promregator.fetcher.MetricsFetcherMetrics;
-import org.cloudfoundry.promregator.rewrite.AbstractMetricFamilySamplesEnricher;
-import org.cloudfoundry.promregator.rewrite.CFMetricFamilySamplesEnricher;
-import org.cloudfoundry.promregator.rewrite.GenericMetricFamilySamplesPrefixRewriter;
-import org.cloudfoundry.promregator.rewrite.MergableMetricFamilySamples;
-import org.cloudfoundry.promregator.scanner.AppInstanceScanner;
 import org.cloudfoundry.promregator.scanner.Instance;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Scope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.context.WebApplicationContext;
 
-import io.prometheus.client.Collector.MetricFamilySamples;
-import io.prometheus.client.CollectorRegistry;
-import io.prometheus.client.Gauge;
 import io.prometheus.client.exporter.common.TextFormat;
 
 /**
  * A spring-framework HTTP REST-server endpoint, compliant to the specification of a Prometheus text (!) metrics endpoint,
  * whose data is being backed by a set of further Prometheus metrics endpoints run on one or several CF apps. 
- * The data is simply aggregated 1:1 without paying attention overlap and redundancy.
  *
  */
 @RestController
 @RequestMapping("/metrics")
-@Scope(value=WebApplicationContext.SCOPE_REQUEST)
-public class MetricsEndpoint {
-	
-	private static final Logger log = Logger.getLogger(MetricsEndpoint.class);
-	
-	@Autowired
-	private AppInstanceScanner appInstanceScanner;
-	
-	@Autowired
-	private ExecutorService metricsFetcherPool;
-	
-	@Autowired
-	private CollectorRegistry collectorRegistry;
-	
-	@Value("${cf.proxyHost:@null}")
-	private String proxyHost;
-	
-	@Value("${cf.proxyPort:0}")
-	private int proxyPort;
-
-	@Value("${promregator.endpoint.maxProcessingTime:5000}")
-	private int maxProcessingTime;
-	
-	@Autowired
-	private PromregatorConfiguration promregatorConfiguration;
-	
-	@Autowired
-	private AuthenticationEnricher ae;
-	
-	private GenericMetricFamilySamplesPrefixRewriter gmfspr = new GenericMetricFamilySamplesPrefixRewriter("promregator");
-	
-	/* own metrics --- specific to this (scraping) request */
-	private CollectorRegistry requestRegistry;
-	
-	// see also https://prometheus.io/docs/instrumenting/writing_exporters/#metrics-about-the-scrape-itself
-	private Gauge scrape_duration;
-	private Gauge up;
-	
-	@PostConstruct
-	public void setupOwnRequestScopedMetrics() {
-		this.requestRegistry = new CollectorRegistry();
-		
-		this.scrape_duration = Gauge.build("promregator_scrape_duration_seconds", "Duration in seconds indicating how long scraping of all metrics took")
-				.register(this.requestRegistry);
-		
-		this.up = Gauge.build("promregator_up", "Indicator, whether the target of promregator is available")
-				.labelNames(CFMetricFamilySamplesEnricher.getEnrichingLabelNames())
-				.register(this.requestRegistry);
-	}
+public class MetricsEndpoint extends AbstractMetricsEndpoint {
 	
 	@RequestMapping(method = RequestMethod.GET, produces=TextFormat.CONTENT_TYPE_004)
 	public String getMetrics() {
-		Instant start = Instant.now();
-		
-		this.up.clear();
-		
-		List<Instance> instanceList = this.appInstanceScanner.determineInstancesFromTargets(this.promregatorConfiguration.getTargets());
-		
-		List<MetricsFetcher> callablesPrep = this.createMetricsFetchers(instanceList);
-		
-		LinkedList<Future<HashMap<String, MetricFamilySamples>>> futures = this.startMetricsFetchers(callablesPrep);
-		
-		MergableMetricFamilySamples mmfs = waitForMetricsFetchers(futures);
-		
-		Instant stop = Instant.now();
-		Duration duration = Duration.between(start, stop);
-		this.scrape_duration.set(duration.toMillis() / 1000.0);
-		
-		// also add our own (global) metrics
-		mmfs.merge(this.gmfspr.determineEnumerationOfMetricFamilySamples(this.collectorRegistry));
-		
-		// add also our own request-specific metrics
-		mmfs.merge(this.gmfspr.determineEnumerationOfMetricFamilySamples(this.requestRegistry));
-		
-		return mmfs.toType004String();
+		return this.handleRequest();
 	}
 
-	private MergableMetricFamilySamples waitForMetricsFetchers(LinkedList<Future<HashMap<String, MetricFamilySamples>>> futures) {
-		long starttime = System.currentTimeMillis();
-		
-		MergableMetricFamilySamples mmfs = new MergableMetricFamilySamples();
-		
-		for (Future<HashMap<String, MetricFamilySamples>> future : futures) {
-			long maxWaitTime = starttime + this.maxProcessingTime - System.currentTimeMillis();
-			
-			try {
-				if (maxWaitTime < 0 && !future.isDone()) {
-					// only process those, which are already completed
-					continue;
-				}
-				HashMap<String, MetricFamilySamples> emfs = future.get(maxWaitTime, TimeUnit.MILLISECONDS);
-				
-				if (emfs != null) {
-					mmfs.merge(emfs);
-				}
-			} catch (InterruptedException e) {
-				continue;
-			} catch (ExecutionException e) {
-				log.warn("Exception thrown while fetching Metrics data from target", e);
-				continue;
-			} catch (TimeoutException e) {
-				log.info("Timeout while fetching metrics data from target", e);
-				continue; // process the other's as well!
-			}
-			
-		}
-		return mmfs;
+	@Override
+	protected boolean isIncludeGlobalMetrics() {
+		return true;
 	}
 
-	private LinkedList<Future<HashMap<String, MetricFamilySamples>>> startMetricsFetchers(List<MetricsFetcher> callablesPrep) {
-		LinkedList<Future<HashMap<String,MetricFamilySamples>>> futures = new LinkedList<>();
-		
-		for (MetricsFetcher mf : callablesPrep) {
-			Future<HashMap<String, MetricFamilySamples>> future = this.metricsFetcherPool.submit(mf);
-			
-			futures.add(future);
-		}
-		return futures;
+	@Override
+	protected List<Instance> filterInstanceList(List<Instance> instanceList) {
+		// all instances shall be processed
+		return instanceList; 
 	}
-
-	protected List<MetricsFetcher> createMetricsFetchers(List<Instance> instanceList) {
-		
-		List<MetricsFetcher> callablesList = new LinkedList<>();
-		for (Instance instance : instanceList) {
-			log.info(String.format("Instance %s", instance.getInstanceId()));
-			
-			Target target = instance.getTarget();
-			String orgName = target.getOrgName();
-			String spaceName = target.getSpaceName();
-			String appName = target.getApplicationName();
-			
-			String accessURL = instance.getAccessUrl();
-			
-			if (accessURL == null) {
-				log.warn(String.format("Unable to retrieve hostname for %s/%s/%s; skipping", orgName, spaceName, appName));
-				continue;
-			}
-			
-			AbstractMetricFamilySamplesEnricher mfse = new CFMetricFamilySamplesEnricher(orgName, spaceName, appName, instance.getInstanceId());
-			MetricsFetcherMetrics mfm = new MetricsFetcherMetrics(mfse, up);
-
-			MetricsFetcher mf = null;
-			
-			if (this.proxyHost != null && this.proxyPort != 0) {
-				mf = new CFMetricsFetcher(accessURL, instance.getInstanceId(), this.ae, mfse, this.proxyHost, this.proxyPort, mfm);
-			} else {
-				mf = new CFMetricsFetcher(accessURL, instance.getInstanceId(), this.ae, mfse, mfm);
-			}
-			callablesList.add(mf);
-		}
-		
-		return callablesList;
-	}
+	
 }
--- src/main/java/org/cloudfoundry/promregator/endpoint/SingleTargetMetricsEndpoint.java
+++ src/main/java/org/cloudfoundry/promregator/endpoint/SingleTargetMetricsEndpoint.java
@@ -1,92 +1,25 @@
 package org.cloudfoundry.promregator.endpoint;
 
-import java.time.Duration;
-import java.time.Instant;
-import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Future;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
 
-import javax.annotation.PostConstruct;
-
-import org.cloudfoundry.promregator.auth.AuthenticationEnricher;
-import org.cloudfoundry.promregator.config.PromregatorConfiguration;
-import org.cloudfoundry.promregator.config.Target;
-import org.cloudfoundry.promregator.fetcher.CFMetricsFetcher;
-import org.cloudfoundry.promregator.fetcher.MetricsFetcher;
-import org.cloudfoundry.promregator.fetcher.MetricsFetcherMetrics;
-import org.cloudfoundry.promregator.rewrite.AbstractMetricFamilySamplesEnricher;
-import org.cloudfoundry.promregator.rewrite.CFMetricFamilySamplesEnricher;
-import org.cloudfoundry.promregator.rewrite.GenericMetricFamilySamplesPrefixRewriter;
-import org.cloudfoundry.promregator.rewrite.MergableMetricFamilySamples;
-import org.cloudfoundry.promregator.scanner.AppInstanceScanner;
 import org.cloudfoundry.promregator.scanner.Instance;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Scope;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.context.WebApplicationContext;
 
-import io.prometheus.client.CollectorRegistry;
-import io.prometheus.client.Gauge;
-import io.prometheus.client.Collector.MetricFamilySamples;
 import io.prometheus.client.exporter.common.TextFormat;
 
 @RestController
 @RequestMapping(SingleTargetMetricsEndpoint.ENDPOINT_PATH+"/{applicationId}/{instanceNumber}")
-@Scope(value=WebApplicationContext.SCOPE_REQUEST)
-public class SingleTargetMetricsEndpoint {
+public class SingleTargetMetricsEndpoint extends AbstractMetricsEndpoint {
 	public static final String ENDPOINT_PATH = "/singleTargetMetrics";
 	
-	@Autowired
-	private AppInstanceScanner appInstanceScanner;
-	
-	@Autowired
-	private PromregatorConfiguration promregatorConfiguration;
-	
-	@Autowired
-	private AuthenticationEnricher ae;
-
-	@Value("${cf.proxyHost:@null}")
-	private String proxyHost;
-	
-	@Value("${cf.proxyPort:0}")
-	private int proxyPort;
-	
-	@Value("${promregator.endpoint.maxProcessingTime:5000}")
-	private int maxProcessingTime;
-	
-	@Autowired
-	private ExecutorService metricsFetcherPool;
-	
-	private GenericMetricFamilySamplesPrefixRewriter gmfspr = new GenericMetricFamilySamplesPrefixRewriter("promregator");
-	
-	/* own metrics --- specific to this (scraping) request */
-	private CollectorRegistry requestRegistry;
-	
-	// see also https://prometheus.io/docs/instrumenting/writing_exporters/#metrics-about-the-scrape-itself
-	private Gauge scrape_duration;
-	private Gauge up;
-	
-	@PostConstruct
-	public void setupOwnRequestScopedMetrics() {
-		this.requestRegistry = new CollectorRegistry();
-		
-		this.scrape_duration = Gauge.build("promregator_scrape_duration_seconds", "Duration in seconds indicating how long scraping of all metrics took")
-				.register(this.requestRegistry);
-		
-		this.up = Gauge.build("promregator_up", "Indicator, whether the target of promregator is available")
-				.labelNames(CFMetricFamilySamplesEnricher.getEnrichingLabelNames())
-				.register(this.requestRegistry);
-	}
+	private String applicationId;
+	private String instanceNumber;
 	
 	@RequestMapping(method = RequestMethod.GET, produces=TextFormat.CONTENT_TYPE_004)
 	public String getMetrics(
@@ -94,13 +27,21 @@ public class SingleTargetMetricsEndpoint {
 			@PathVariable String instanceNumber
 			) {
 		
-		Instant start = Instant.now();
+		this.applicationId = applicationId;
+		this.instanceNumber = instanceNumber;
 		
-		this.up.clear();
-		
+		return this.handleRequest();
+	}
+
+	@Override
+	protected boolean isIncludeGlobalMetrics() {
+		// NB: This is done by PromregatorMetricsEndpoint in this scenario instead.
+		return false;
+	}
+
+	@Override
+	protected List<Instance> filterInstanceList(List<Instance> instanceList) {
 		String instanceId = String.format("%s:%s", applicationId, instanceNumber);
-		
-		List<Instance> instanceList = this.appInstanceScanner.determineInstancesFromTargets(this.promregatorConfiguration.getTargets());
 
 		Instance selectedInstance = null;
 		for (Instance instance : instanceList) {
@@ -113,52 +54,10 @@ public class SingleTargetMetricsEndpoint {
 		if (selectedInstance == null) {
 			throw new HttpClientErrorException(HttpStatus.NOT_FOUND);
 		}
-		
-		Target target = selectedInstance.getTarget();
-		String orgName = target.getOrgName();
-		String spaceName = target.getSpaceName();
-		String appName = target.getApplicationName();
 
-		String accessURL = selectedInstance.getAccessUrl();
-
-		AbstractMetricFamilySamplesEnricher mfse = new CFMetricFamilySamplesEnricher(orgName, spaceName, appName, selectedInstance.getInstanceId());
-		MetricsFetcherMetrics mfm = new MetricsFetcherMetrics(mfse, this.up);
+		List<Instance> response = new LinkedList<Instance>();
+		response.add(selectedInstance);
 		
-		MetricsFetcher mf = null;
-		if (this.proxyHost != null && this.proxyPort != 0) {
-			mf = new CFMetricsFetcher(accessURL, selectedInstance.getInstanceId(), this.ae, mfse, this.proxyHost, this.proxyPort, mfm);
-		} else {
-			mf = new CFMetricsFetcher(accessURL, selectedInstance.getInstanceId(), this.ae, mfse, mfm);
-		}
-		
-		Future<HashMap<String, MetricFamilySamples>> future = this.metricsFetcherPool.submit(mf);
-		
-		HashMap<String, MetricFamilySamples> resultMap = null;
-		try {
-			resultMap = future.get(this.maxProcessingTime, TimeUnit.MILLISECONDS);
-		} catch (InterruptedException e) {
-			// TODO logging
-			throw new HttpClientErrorException(HttpStatus.INTERNAL_SERVER_ERROR);
-		} catch (ExecutionException e) {
-			// TODO logging
-			throw new HttpClientErrorException(HttpStatus.INTERNAL_SERVER_ERROR);
-		} catch (TimeoutException e) {
-			// TODO logging
-			throw new HttpClientErrorException(HttpStatus.BAD_GATEWAY);
-		}
-		
-		Instant stop = Instant.now();
-		Duration duration = Duration.between(start, stop);
-		this.scrape_duration.set(duration.toMillis() / 1000.0);
-		
-		MergableMetricFamilySamples mmfs = new MergableMetricFamilySamples();
-		mmfs.merge(resultMap);
-		
-		// add also our own request-specific metrics
-		mmfs.merge(this.gmfspr.determineEnumerationOfMetricFamilySamples(this.requestRegistry));
-		
-		return mmfs.toType004String();
+		return response;
 	}
-
-	
 }
