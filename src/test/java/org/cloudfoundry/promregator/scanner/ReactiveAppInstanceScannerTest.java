f9f262086a85870e6cfcaba928e2fe28939d0a11
Nico Schmoigl <github@schmoigl-online.de>
2018-03-24 23:00:41 +0100

    Coverage on Internal Metrics requires activation via properties

--- src/test/java/org/cloudfoundry/promregator/scanner/ReactiveAppInstanceScannerTest.java
+++ src/test/java/org/cloudfoundry/promregator/scanner/ReactiveAppInstanceScannerTest.java
@@ -10,10 +10,12 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = MockedReactiveAppInstanceScannerSpringApplication.class)
+@TestPropertySource(locations="default.properties")
 public class ReactiveAppInstanceScannerTest {
 
 	@Autowired
