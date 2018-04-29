00fa2ae94e143abf18a45975011165c9ba9ce752
Nico Schmoigl <github@schmoigl-online.de>
2018-04-29 21:42:45 +0200

    Unit test for new Invalidate Cache endpoint

---
+++ src/test/java/org/cloudfoundry/promregator/endpoint/InvalidateCacheEndpointTest.java
@@ -0,0 +1,40 @@
+package org.cloudfoundry.promregator.endpoint;
+
+import org.cloudfoundry.promregator.scanner.AppInstanceScanner;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = MockedAppInstanceScannerEndpointSpringApplication.class)
+@TestPropertySource(locations="default.properties")
+public class InvalidateCacheEndpointTest {
+
+	@Autowired
+	private InvalidateCacheEndpoint subject;
+	
+	@Autowired
+	private AppInstanceScanner appInstanceScanner;
+	
+	@Test
+	public void testInvalidateCacheAll() {
+		Assert.assertNotNull(subject);
+		
+		ResponseEntity<String> response = subject.invalidateCache(true, true, true);
+		
+		Assert.assertEquals(HttpStatus.NO_CONTENT, response.getStatusCode());
+		
+		MockedAppInstanceScannerEndpointSpringApplication.MockedReactiveAppInstanceScanner ais = (MockedAppInstanceScannerEndpointSpringApplication.MockedReactiveAppInstanceScanner) this.appInstanceScanner;
+		
+		Assert.assertTrue(ais.isAppInvalidated());
+		Assert.assertTrue(ais.isSpaceInvalidated());
+		Assert.assertTrue(ais.isOrgInvalidated());
+	}
+
+}
