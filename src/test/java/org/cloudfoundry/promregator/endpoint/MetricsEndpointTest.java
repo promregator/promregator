e7508be44242aaf0fe55560c816207f116fbf6d0
Nico Schmoigl <github@schmoigl-online.de>
2018-05-08 23:26:28 +0200

    Fix Issue #52: Unit test coverage for Single Endpoint Scraping

--- src/test/java/org/cloudfoundry/promregator/endpoint/MetricsEndpointTest.java
+++ src/test/java/org/cloudfoundry/promregator/endpoint/MetricsEndpointTest.java
@@ -12,6 +12,7 @@ import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import io.prometheus.client.Collector.MetricFamilySamples;
+import io.prometheus.client.Collector.MetricFamilySamples.Sample;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = MockedMetricsEndpointSpringApplication.class)
@@ -35,6 +36,30 @@ public class MetricsEndpointTest {
 		
 		Assert.assertNotNull(mapMFS.get("metric_unittestapp"));
 		Assert.assertNotNull(mapMFS.get("metric_unittestapp2"));
+	}
+	
+	@Test
+	public void testIssue52() {
+		Assert.assertNotNull(subject);
+		
+		String response = subject.getMetrics();
+		
+		Assert.assertNotNull(response);
+		Assert.assertNotEquals("", response);
+		
+		TextFormat004Parser parser = new TextFormat004Parser(response);
+		HashMap<String, MetricFamilySamples> mapMFS = parser.parse();
+		
+		Assert.assertNotNull(mapMFS.get("metric_unittestapp"));
+		Assert.assertNotNull(mapMFS.get("metric_unittestapp2"));
+		
+		MetricFamilySamples mfs = mapMFS.get("promregator_scrape_duration_seconds");
+		Assert.assertNotNull(mfs);
+		Assert.assertEquals(1, mfs.samples.size());
+		
+		Sample sample = mfs.samples.get(0);
+		Assert.assertTrue(sample.labelNames.isEmpty());
+		Assert.assertTrue(sample.labelValues.isEmpty());
 	}
 
 }
