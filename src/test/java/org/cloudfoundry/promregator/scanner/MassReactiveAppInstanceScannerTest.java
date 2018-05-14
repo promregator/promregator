375bc90604f5ad705525bc42776d49fc00a36379
Nico Schmoigl <github@schmoigl-online.de>
2018-05-14 23:28:20 +0200

    Performance test with large mass of instances

---
+++ src/test/java/org/cloudfoundry/promregator/scanner/MassReactiveAppInstanceScannerTest.java
@@ -0,0 +1,64 @@
+package org.cloudfoundry.promregator.scanner;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.cloudfoundry.promregator.config.Target;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import io.prometheus.client.CollectorRegistry;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = MockedMassReactiveAppInstanceScannerSpringApplication.class)
+@TestPropertySource(locations="default.properties")
+public class MassReactiveAppInstanceScannerTest {
+
+	@Autowired
+	private AppInstanceScanner appInstanceScanner;
+	
+	@AfterClass
+	public static void releaseInternalMetrics() {
+		CollectorRegistry.defaultRegistry.clear();
+	}
+	
+	@Test
+	public void testPerformance() {
+		List<Target> targets = new LinkedList<>();
+		
+		Target t = null;
+		
+		final int numberOfApps = 10000;
+		
+		for (int i = 0;i<numberOfApps;i++) {
+			t = new Target();
+			t.setOrgName("unittestorg");
+			t.setSpaceName("unittestspace");
+			t.setApplicationName("testapp"+i);
+			t.setPath("/testpath");
+			t.setProtocol("http");
+			targets.add(t);
+		}
+		
+		Instant start = Instant.now();
+		
+		List<Instance> result = this.appInstanceScanner.determineInstancesFromTargets(targets);
+		
+		Instant stop = Instant.now();
+		
+		Assert.assertEquals(numberOfApps*10, result.size());
+		
+		// test to be faster than 3 seconds
+		Duration d = Duration.between(start, stop);
+		Assert.assertTrue(d.minusSeconds(3).isNegative());
+	}
+
+}
---
+++ src/test/java/org/cloudfoundry/promregator/scanner/MockedMassReactiveAppInstanceScannerSpringApplication.java
@@ -0,0 +1,27 @@
+package org.cloudfoundry.promregator.scanner;
+
+import org.cloudfoundry.promregator.cfaccessor.CFAccessor;
+import org.cloudfoundry.promregator.cfaccessor.CFAccessorMassMock;
+import org.cloudfoundry.promregator.internalmetrics.InternalMetrics;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MockedMassReactiveAppInstanceScannerSpringApplication {
+	
+	@Bean
+	public InternalMetrics internalMetrics() {
+		return new InternalMetrics();
+	}
+
+	@Bean
+	public CFAccessor cfAccessor() {
+		return new CFAccessorMassMock(10);
+	}
+	
+	@Bean
+	public AppInstanceScanner appInstanceScanner() {
+		return new ReactiveAppInstanceScanner();
+	}
+	
+}
