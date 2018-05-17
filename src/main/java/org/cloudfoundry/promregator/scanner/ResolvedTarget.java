cf3de83d4d26325a952fcde12fdd521a02b21df1
Nico Schmoigl <github@schmoigl-online.de>
2018-05-17 12:10:56 +0200

    Improve unit test coverage of ResolvedTarget

--- src/main/java/org/cloudfoundry/promregator/scanner/ResolvedTarget.java
+++ src/main/java/org/cloudfoundry/promregator/scanner/ResolvedTarget.java
@@ -133,7 +133,6 @@ public class ResolvedTarget {
 	 * @see java.lang.Object#toString()
 	 */
 	@Override
-	// TODO requires unit test coverage
 	public String toString() {
 		StringBuilder builder = new StringBuilder();
 		builder.append("ResolvedTarget [orgName=");
