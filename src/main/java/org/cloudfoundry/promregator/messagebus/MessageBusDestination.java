41a92ff0b6ea846825013da17b58ea190d8e559f
Nico Schmoigl <github@schmoigl-online.de>
2018-05-19 20:52:12 +0200

    PMD: Remove unnecessary public modifier

--- src/main/java/org/cloudfoundry/promregator/messagebus/MessageBusDestination.java
+++ src/main/java/org/cloudfoundry/promregator/messagebus/MessageBusDestination.java
@@ -1,7 +1,7 @@
 package org.cloudfoundry.promregator.messagebus;
 
 public interface MessageBusDestination {
-	public final static String PREFIX = "org.cloudfoundry.promregator.";
+	final static String PREFIX = "org.cloudfoundry.promregator.";
 	
-	public final static String DISCOVERER_INSTANCE_REMOVED = PREFIX + "instanceRemoved";
+	final static String DISCOVERER_INSTANCE_REMOVED = PREFIX + "instanceRemoved";
 }
