1d92ff47d2aa87657436ee22a27b9cfebc8be4e1
Nico Schmoigl <github@schmoigl-online.de>
2018-08-03 21:58:11 +0200

    Adding test of timeout/retry/doOnError handling (Reactor)

---
+++ src/test/java/org/cloudfoundry/promregator/MonoErrorHandlingTest.java
@@ -0,0 +1,34 @@
+package org.cloudfoundry.promregator;
+
+import java.time.Duration;
+import java.time.Instant;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import reactor.core.publisher.Mono;
+
+public class MonoErrorHandlingTest {
+	@Test
+	public void test() {
+		boolean raised = false;
+		try {
+			Mono.just(1).map(v -> {
+				System.err.println("Waiting " + Instant.now());
+				try {
+					Thread.sleep(200);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				System.err.println("returning " + Instant.now());
+				return v;
+			}).timeout(Duration.ofMillis(100)).retry(2).doOnError(x -> {
+				System.err.println("Exception Handling " + Instant.now());
+				x.printStackTrace(System.err);
+			}).block();
+		} catch (RuntimeException re) {
+			raised = true;
+		}
+		Assert.assertTrue(raised);
+	}
+}
