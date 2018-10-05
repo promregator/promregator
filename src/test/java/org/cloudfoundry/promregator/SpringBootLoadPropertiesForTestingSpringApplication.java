883fdf2e971b0edcb397cf17b041d819e2801c19
Nico Schmoigl <github@schmoigl-online.de>
2018-10-05 14:27:24 +0200

    Test environment for adding yaml documents in JUnit Spring environment

---
+++ src/test/java/org/cloudfoundry/promregator/SpringBootLoadPropertiesForTestingSpringApplication.java
@@ -0,0 +1,24 @@
+package org.cloudfoundry.promregator;
+
+import org.junit.Assert;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("springBootLoadPopertiesForTesting")
+public class SpringBootLoadPropertiesForTestingSpringApplication {
+	@Value("${dummy.value:false}")
+	private boolean dummyValue;
+	
+	@Bean
+	public Object anything() {
+		Assert.assertTrue(dummyValue);
+		return new Object();
+	}
+	
+	public void check() {
+		Assert.assertTrue(dummyValue);
+	}
+}
