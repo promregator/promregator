c52b31831b5807f21286a662cdd09981b098b749
Nico Schmoigl <github@schmoigl-online.de>
2018-11-03 00:32:26 +0100

    Unit test for CF[InstanceOnly|OwnMetrics]MetricFamilySamplesEnricher

--- src/test/java/org/cloudfoundry/promregator/rewrite/MetricFamilySamplesEnricherTest.java
+++ src/test/java/org/cloudfoundry/promregator/rewrite/MetricFamilySamplesEnricherTest.java
@@ -21,7 +21,7 @@ public class MetricFamilySamplesEnricherTest {
 	}
 
 	@Test
-	public void testSimple() {
+	public void testAllLabelsSimple() {
 		AbstractMetricFamilySamplesEnricher subject = new CFAllLabelsMetricFamilySamplesEnricher("testOrgName", "testSpaceName", "testComponent", "testInstance:42");
 
 		List<Sample> samples = new LinkedList<>();
@@ -66,6 +66,90 @@ public class MetricFamilySamplesEnricherTest {
 		Assert.assertEquals("testComponent", labelValues[4]);
 		Assert.assertEquals("testInstance:42", labelValues[5]);
 		Assert.assertEquals("42", labelValues[6]);
+	}
+	
+	@Test
+	public void testOwnMetricsSimple() {
+		AbstractMetricFamilySamplesEnricher subject = new CFOwnMetricsMetricFamilySamplesEnricher("testOrgName", "testSpaceName", "testComponent", "testInstance:42");
+
+		List<Sample> samples = new LinkedList<>();
+		Sample s = new Sample("dummyname", Arrays.asList(new String[] { "labelName" }), Arrays.asList(new String[] {"labelValue"}), 1.0);
+		samples.add(s);
+		
+		MetricFamilySamples mfs = new MetricFamilySamples("dummyname", Type.GAUGE, "dummyHelp", samples);
+		
+		HashMap<String, MetricFamilySamples> map = new HashMap<>();
+		map.put("metricName", mfs);
+		
+		HashMap<String, MetricFamilySamples> result = subject.determineEnumerationOfMetricFamilySamples(map);
+		
+		Assert.assertEquals(1, result.size());
+		
+		MetricFamilySamples testMFS = result.get("metricName");
+		Assert.assertNotNull(testMFS);
+		
+		Assert.assertEquals(1, testMFS.samples.size());
+		
+		Sample testSample = testMFS.samples.get(0);
+		Assert.assertNotNull(testSample);
+		
+		List<String> labelNamesList = testSample.labelNames;
+		String[] labelNames = labelNamesList.toArray(new String[0]);
+		
+		Assert.assertEquals("labelName", labelNames[0]);
+		Assert.assertEquals("org_name", labelNames[1]);
+		Assert.assertEquals("space_name", labelNames[2]);
+		Assert.assertEquals("app_name", labelNames[3]);
+		Assert.assertEquals("cf_instance_id", labelNames[4]);
+		Assert.assertEquals("cf_instance_number", labelNames[5]);
+		
+		List<String> labelValuesList = testSample.labelValues;
+		String[] labelValues = labelValuesList.toArray(new String[0]);
+
+		Assert.assertEquals("labelValue", labelValues[0]);
+		Assert.assertEquals("testOrgName", labelValues[1]);
+		Assert.assertEquals("testSpaceName", labelValues[2]);
+		Assert.assertEquals("testComponent", labelValues[3]);
+		Assert.assertEquals("testInstance:42", labelValues[4]);
+		Assert.assertEquals("42", labelValues[5]);
+	}
+
+	@Test
+	public void testInstanceOnlySimple() {
+		AbstractMetricFamilySamplesEnricher subject = new CFInstanceOnlyMetricFamilySamplesEnricher("testInstance:42");
+
+		List<Sample> samples = new LinkedList<>();
+		Sample s = new Sample("dummyname", Arrays.asList(new String[] { "labelName" }), Arrays.asList(new String[] {"labelValue"}), 1.0);
+		samples.add(s);
+		
+		MetricFamilySamples mfs = new MetricFamilySamples("dummyname", Type.GAUGE, "dummyHelp", samples);
+		
+		HashMap<String, MetricFamilySamples> map = new HashMap<>();
+		map.put("metricName", mfs);
+		
+		HashMap<String, MetricFamilySamples> result = subject.determineEnumerationOfMetricFamilySamples(map);
+		
+		Assert.assertEquals(1, result.size());
+		
+		MetricFamilySamples testMFS = result.get("metricName");
+		Assert.assertNotNull(testMFS);
+		
+		Assert.assertEquals(1, testMFS.samples.size());
+		
+		Sample testSample = testMFS.samples.get(0);
+		Assert.assertNotNull(testSample);
+		
+		List<String> labelNamesList = testSample.labelNames;
+		String[] labelNames = labelNamesList.toArray(new String[0]);
+		
+		Assert.assertEquals("labelName", labelNames[0]);
+		Assert.assertEquals("instance", labelNames[1]);
+		
+		List<String> labelValuesList = testSample.labelValues;
+		String[] labelValues = labelValuesList.toArray(new String[0]);
+
+		Assert.assertEquals("labelValue", labelValues[0]);
+		Assert.assertEquals("testInstance:42", labelValues[1]);
 	}
 
 }
