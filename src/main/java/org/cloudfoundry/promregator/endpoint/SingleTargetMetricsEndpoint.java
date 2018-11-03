6a2c0a4792e6efdc3fec4d76f984045cf27cf703
Nico Schmoigl <github@schmoigl-online.de>
2018-11-03 11:48:43 +0100

    Fix imports

--- src/main/java/org/cloudfoundry/promregator/endpoint/SingleTargetMetricsEndpoint.java
+++ src/main/java/org/cloudfoundry/promregator/endpoint/SingleTargetMetricsEndpoint.java
@@ -4,7 +4,6 @@ import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.cloudfoundry.promregator.rewrite.CFAllLabelsMetricFamilySamplesEnricher;
 import org.cloudfoundry.promregator.rewrite.CFOwnMetricsMetricFamilySamplesEnricher;
 import org.cloudfoundry.promregator.scanner.Instance;
 import org.cloudfoundry.promregator.scanner.ResolvedTarget;
