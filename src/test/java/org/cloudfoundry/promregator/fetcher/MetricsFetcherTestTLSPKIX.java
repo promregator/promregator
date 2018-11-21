739d00bb1b4ced4aa42bbaeb0bdcbe9c713694d4
Nico Schmoigl <github@schmoigl-online.de>
2018-11-21 22:25:54 +0100

    First negative text: PKIX is raised if self-signed only

---
+++ src/test/java/org/cloudfoundry/promregator/fetcher/MetricsFetcherTestTLSPKIX.java
@@ -0,0 +1,77 @@
+package org.cloudfoundry.promregator.fetcher;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.UUID;
+
+import org.cloudfoundry.promregator.JUnitTestUtils;
+import org.cloudfoundry.promregator.mockServer.MetricsEndpointMockServerTLS;
+import org.cloudfoundry.promregator.rewrite.CFAllLabelsMetricFamilySamplesEnricher;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import io.prometheus.client.Collector.MetricFamilySamples;
+
+public class MetricsFetcherTestTLSPKIX {
+
+	private static final String DUMMY_METRICS_LIST = "# HELP dummy This is a dummy metric\n"+
+			"# TYPE dummy counter\n"+
+			"dummy 42 1395066363000";
+	private MetricsEndpointMockServerTLS mems;
+	
+	public MetricsFetcherTestTLSPKIX() {
+	}
+	
+	@Before
+	public void startUpMetricsEndpointServer() throws IOException {
+		this.mems = new MetricsEndpointMockServerTLS();
+		this.mems.start();
+	}
+	
+	@After
+	public void tearDownMetricsEndpointServer() {
+		this.mems.stop();
+	}
+
+	@AfterClass
+	public static void cleanupEnvironment() {
+		JUnitTestUtils.cleanUpAll();
+	}
+	
+	private static class NullMetricFamilySamplesEnricher extends CFAllLabelsMetricFamilySamplesEnricher {
+
+		public NullMetricFamilySamplesEnricher(String orgName, String spaceName, String appName, String instance) {
+			super(orgName, spaceName, appName, instance);
+		}
+
+		@Override
+		public HashMap<String, MetricFamilySamples> determineEnumerationOfMetricFamilySamples(HashMap<String, MetricFamilySamples> mfs) {
+			return mfs;
+		}
+	}
+	
+	@Test
+	public void testPKIXErrorOnSelfSignedCertificateInDefaultMode() throws Exception {
+		String instanceId = "abcd:4";
+		NullMetricFamilySamplesEnricher dummymfse = new NullMetricFamilySamplesEnricher("dummy", "dummy", "dummy", "dummy:0");
+		List<String> labelValues = dummymfse.getEnrichedLabelValues(new LinkedList<>());
+		String[] ownTelemetryLabelValues = labelValues.toArray(new String[0]);
+		
+		MetricsFetcherMetrics mfm = new MetricsFetcherMetrics(ownTelemetryLabelValues, false);
+		UUID currentUUID = UUID.randomUUID();
+		CFMetricsFetcher subject = new CFMetricsFetcher("https://localhost:9003/metrics", instanceId, null, dummymfse, mfm, null, currentUUID);
+		
+		this.mems.getMetricsEndpointHandler().setResponse(DUMMY_METRICS_LIST);
+		
+		HashMap<String, MetricFamilySamples> response = subject.call();
+		
+		Assert.assertNull(response);
+	}
+	
+
+}
---
+++ src/test/java/org/cloudfoundry/promregator/mockServer/MetricsEndpointMockServerTLS.java
@@ -0,0 +1,115 @@
+package org.cloudfoundry.promregator.mockServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
+
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManagerFactory;
+
+import org.junit.Assert;
+
+import com.sun.net.httpserver.HttpsConfigurator;
+import com.sun.net.httpserver.HttpsParameters;
+import com.sun.net.httpserver.HttpsServer;
+
+@SuppressWarnings("restriction")
+public class MetricsEndpointMockServerTLS {
+	private HttpsServer server;
+
+	protected int port = 9003;
+
+	private DefaultMetricsEndpointHttpHandler myHandler;
+
+	public MetricsEndpointMockServerTLS() {
+	}
+
+	public void start() throws IOException {
+		InetSocketAddress bindAddress = new InetSocketAddress("127.0.0.1", this.port);
+		this.server = HttpsServer.create(bindAddress, 0);
+		this.server.setHttpsConfigurator(new HttpsConfigurator(this.createSslContext()) {
+			public void configure(HttpsParameters params) {
+				SSLContext c = getSSLContext();
+				params.setSSLParameters(c.getDefaultSSLParameters());
+			}
+		});
+
+		this.server.createContext("/metrics", this.getMetricsEndpointHandler());
+
+		this.server.start();
+	}
+
+	private SSLContext createSslContext() {
+		final char[] passphrase = "unittest".toCharArray();
+
+		KeyStore ks = null;
+		try {
+			ks = KeyStore.getInstance("JKS");
+		} catch (KeyStoreException e) {
+			Assert.fail("Unable to set up JKS Keystore");
+		}
+		try {
+			ks.load(this.getClass().getResourceAsStream("selfsigned.jks"), passphrase);
+		} catch (NoSuchAlgorithmException | CertificateException | IOException e) {
+			Assert.fail("Unable to load selfsigned keystore");
+		}
+
+		KeyManagerFactory kmf = null;
+		try {
+			kmf = KeyManagerFactory.getInstance("SunX509");
+		} catch (NoSuchAlgorithmException e) {
+			Assert.fail("Unable to retrieve KeyManagerFactory");
+		}
+		try {
+			kmf.init(ks, passphrase);
+		} catch (UnrecoverableKeyException | KeyStoreException | NoSuchAlgorithmException e) {
+			Assert.fail("Unable to initialize KeyManagerFactory");
+		}
+
+		TrustManagerFactory tmf = null;
+		try {
+			tmf = TrustManagerFactory.getInstance("SunX509");
+		} catch (NoSuchAlgorithmException e) {
+			Assert.fail("Unable to retrieve TrustManagerFactory");
+		}
+		
+		try {
+			tmf.init(ks);
+		} catch (KeyStoreException e) {
+			Assert.fail("Unable to initialize TrustManagerFactory");
+		}
+		
+		SSLContext ssl = null;
+		try {
+			ssl = SSLContext.getInstance("TLS");
+		} catch (NoSuchAlgorithmException e) {
+			Assert.fail("Unable to retrieve SSLContext");
+		}
+		try {
+			ssl.init(kmf.getKeyManagers(), tmf.getTrustManagers(), null);
+		} catch (KeyManagementException e) {
+			Assert.fail("Unable to initialize SSLContext");
+		}
+		
+		return ssl;
+	}
+
+	public DefaultMetricsEndpointHttpHandler getMetricsEndpointHandler() {
+		if (this.myHandler != null) {
+			return myHandler;
+		}
+
+		this.myHandler = new DefaultMetricsEndpointHttpHandler();
+		return this.myHandler;
+	}
+
+	public void stop() {
+		this.server.stop(1);
+	}
+}
