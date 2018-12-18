88dc909b3fb3c3becdf6d337d8a73b0c816d3488
Nico Schmoigl <github@schmoigl-online.de>
2018-12-18 22:40:14 +0100

    Generalize Paging Approach

---
+++ src/main/java/org/cloudfoundry/promregator/cfaccessor/PaginatedRequestGeneratorFunction.java
@@ -0,0 +1,10 @@
+package org.cloudfoundry.promregator.cfaccessor;
+
+import org.cloudfoundry.client.v2.OrderDirection;
+import org.cloudfoundry.client.v2.PaginatedRequest;
+
+@FunctionalInterface
+public interface PaginatedRequestGeneratorFunction<T extends PaginatedRequest> {
+	// for the idea, see also https://stackoverflow.com/a/27872395 
+	public T apply(OrderDirection orderDirection, int resultsPerPage, int pageNumber);
+}
