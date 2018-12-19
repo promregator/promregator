1046a15f92d80df8f2d88692c0ca82ab0a87b653
Nico Schmoigl <github@schmoigl-online.de>
2018-12-19 22:45:29 +0100

    Coverage on errors raised for ReactiveCFPaginatedRequestFetcher

--- src/test/java/org/cloudfoundry/promregator/cfaccessor/ReactiveCFPaginatedRequestFetcherTest.java
+++ src/test/java/org/cloudfoundry/promregator/cfaccessor/ReactiveCFPaginatedRequestFetcherTest.java
@@ -1,6 +1,7 @@
 package org.cloudfoundry.promregator.cfaccessor;
 
 import java.util.LinkedList;
+import java.util.concurrent.TimeoutException;
 
 import org.cloudfoundry.client.v2.organizations.ListOrganizationsRequest;
 import org.cloudfoundry.client.v2.organizations.ListOrganizationsResponse;
@@ -10,6 +11,7 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.mockito.Mockito;
 
+import reactor.core.Exceptions;
 import reactor.core.publisher.Mono;
 
 public class ReactiveCFPaginatedRequestFetcherTest {
@@ -108,5 +110,99 @@ public class ReactiveCFPaginatedRequestFetcherTest {
 		Assert.assertNotNull (subjectResponse.getResources());
 		Assert.assertTrue (2*100 == subjectResponse.getResources().size());
 	}
+	
+	@Test
+	public void testWithContentTwoPagesSecondWithoutItems() {
+		ReactiveCFPaginatedRequestFetcher subject = new ReactiveCFPaginatedRequestFetcher(this.internalMetricsMocked);
+		
+		Mono<ListOrganizationsResponse> subjectResponseMono = subject.performGenericPagedRetrieval("unittest", "unittest", "nokey", requestGenerator, request -> {
+			LinkedList<OrganizationResource> list = new LinkedList<>();
+			
+			if (request.getPage() == 1) {
+				for (int i = 0;i<request.getResultsPerPage(); i++) {
+					list.add(OrganizationResource.builder().build());
+				}
+			}
+			
+			ListOrganizationsResponse response = ListOrganizationsResponse.builder()
+					.resources(list)
+					.totalPages(2)
+					.totalResults( (2 - request.getPage()) * request.getResultsPerPage()) // that's tricky&ugly, but correct: It would be the answer, if suddenly the items on the 2nd page vanished
+					.build();
+			
+			return Mono.just(response);
+		}, 100, responseGenerator);
+		
+		ListOrganizationsResponse subjectResponse = subjectResponseMono.block();
+		Assert.assertTrue (2 == subjectResponse.getTotalPages());
+		Assert.assertTrue (1*100 == subjectResponse.getTotalResults());
+		Assert.assertNotNull (subjectResponse.getResources());
+		Assert.assertTrue (1*100 == subjectResponse.getResources().size());
+	}
+	
+	@Test
+	public void testWithContentTimeoutOnFirstPage() {
+		ReactiveCFPaginatedRequestFetcher subject = new ReactiveCFPaginatedRequestFetcher(this.internalMetricsMocked);
+		
+		Mono<ListOrganizationsResponse> subjectResponseMono = subject.performGenericPagedRetrieval("unittest", "unittest", "nokey", requestGenerator, request -> {
+			return Mono.error(new TimeoutException());
+		}, 100, responseGenerator);
+		
+		ListOrganizationsResponse fallback = ListOrganizationsResponse.builder().build();
+		
+		ListOrganizationsResponse subjectResponse = subjectResponseMono.doOnError(e -> {
+			Assert.assertTrue(Exceptions.unwrap(e) instanceof TimeoutException);
+		}).onErrorReturn(fallback).block();
+		Assert.assertEquals(fallback, subjectResponse);
+	}
+	
+	@Test
+	public void testWithContentTimeoutOnSecondPage() {
+		ReactiveCFPaginatedRequestFetcher subject = new ReactiveCFPaginatedRequestFetcher(this.internalMetricsMocked);
+		
+		Mono<ListOrganizationsResponse> subjectResponseMono = subject.performGenericPagedRetrieval("unittest", "unittest", "nokey", requestGenerator, request -> {
+			if (request.getPage() == 2) {
+				return Mono.error(new TimeoutException());
+			}
+			
+			LinkedList<OrganizationResource> list = new LinkedList<>();
+			
+			if (request.getPage() == 1) {
+				for (int i = 0;i<request.getResultsPerPage(); i++) {
+					list.add(OrganizationResource.builder().build());
+				}
+			}
+			
+			ListOrganizationsResponse response = ListOrganizationsResponse.builder()
+					.resources(list)
+					.totalPages(2)
+					.totalResults(2 * request.getResultsPerPage()) 
+					.build();
+			
+			return Mono.just(response);
+		}, 100, responseGenerator);
+		
+		ListOrganizationsResponse fallback = ListOrganizationsResponse.builder().build();
+		
+		ListOrganizationsResponse subjectResponse = subjectResponseMono.doOnError(e -> {
+			Assert.assertTrue(Exceptions.unwrap(e) instanceof TimeoutException);
+		}).onErrorReturn(fallback).block();
+		Assert.assertEquals(fallback, subjectResponse);
+	}
 
+	@Test
+	public void testWithContentGeneralException() {
+		ReactiveCFPaginatedRequestFetcher subject = new ReactiveCFPaginatedRequestFetcher(this.internalMetricsMocked);
+		
+		Mono<ListOrganizationsResponse> subjectResponseMono = subject.performGenericPagedRetrieval("unittest", "unittest", "nokey", requestGenerator, request -> {
+			return Mono.error(new Exception());
+		}, 100, responseGenerator);
+		
+		ListOrganizationsResponse fallback = ListOrganizationsResponse.builder().build();
+		
+		ListOrganizationsResponse subjectResponse = subjectResponseMono.doOnError(e -> {
+			Assert.assertTrue(Exceptions.unwrap(e) instanceof Exception);
+		}).onErrorReturn(fallback).block();
+		Assert.assertEquals(fallback, subjectResponse);
+	}
 }
