e268363feb9aa88717d07ba9ca7c586aa0cdeb64
Nico Schmoigl <github@schmoigl-online.de>
2020-01-08 22:58:03 +0100

    Add unit test with a second encryption key

---
+++ src/test/java/org/cloudfoundry/promregator/SpringBootLoadPropertiesForTestingOtherKey.java
@@ -0,0 +1,44 @@
+package org.cloudfoundry.promregator;
+
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.ConfigFileApplicationContextInitializer;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@SpringBootTest(classes = { SpringBootLoadPropertiesForTestingSpringApplication.class })
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(initializers = { ConfigFileApplicationContextInitializer.class })
+@ActiveProfiles(profiles= {"springBootLoadPropertiesForTestingOtherKey"})
+@TestPropertySource(properties = "encrypt.key=someotherkey")
+public class SpringBootLoadPropertiesForTestingOtherKey {
+
+	@Autowired
+	private SpringBootLoadPropertiesForTestingSpringApplication springBootLoadPropertiesForTestingSpringApplication;
+
+	@AfterAll
+	public static void cleanupEnvironment() {
+		JUnitTestUtils.cleanUpAll();
+	}
+	
+	@Test
+	public void testContextLoads() {
+		springBootLoadPropertiesForTestingSpringApplication.check();
+		assertThat(true).as("context loads").isTrue();
+	}
+
+	@Test
+	public void testContextLoadsWithEncryptedValue() {
+		String secretValue = springBootLoadPropertiesForTestingSpringApplication.getSecretValue();
+		assertThat(secretValue).as("passwords do not match but should").isEqualTo("myothersecret");
+	}
+}
--- src/test/java/org/cloudfoundry/promregator/SpringBootLoadPropertiesForTestingSpringApplication.java
+++ src/test/java/org/cloudfoundry/promregator/SpringBootLoadPropertiesForTestingSpringApplication.java
@@ -4,10 +4,8 @@ import org.junit.Assert;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
 @Configuration
-@Profile("springBootLoadPropertiesForTesting")
 public class SpringBootLoadPropertiesForTestingSpringApplication {
 	@Value("${dummy.value:false}")
 	private boolean dummyValue;
