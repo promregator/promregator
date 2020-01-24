7e76931104d5bcf37f9b377180ed2e7c99022cbb
Nico Schmoigl <github@schmoigl-online.de>
2020-01-24 23:36:01 +0100

    SonarQube: private constructor for constant interface replacement
    Conflicts:
    	src/main/java/org/cloudfoundry/promregator/ExitCodes.java
    	src/main/java/org/cloudfoundry/promregator/endpoint/EndpointConstants.java

--- src/main/java/org/cloudfoundry/promregator/endpoint/EndpointConstants.java
+++ src/main/java/org/cloudfoundry/promregator/endpoint/EndpointConstants.java
@@ -1,11 +1,15 @@
 package org.cloudfoundry.promregator.endpoint;
 
-public interface EndpointConstants {
-	String ENDPOINT_PATH_DISCOVERY = "/discovery";
-	String ENDPOINT_PATH_SINGLE_ENDPOINT_SCRAPING = "/metrics";
-	String ENDPOINT_PATH_SINGLE_TARGET_SCRAPING = "/singleTargetMetrics";
-	String ENDPOINT_PATH_PROMREGATOR_METRICS = "/promregatorMetrics";
-	String ENDPOINT_PATH_CACHE_INVALIDATION = "/cache/invalidate";
+public final class EndpointConstants {
+	private EndpointConstants() {
+		throw new IllegalStateException("Should never be called");
+	}
+	
+	public static final String ENDPOINT_PATH_DISCOVERY = "/discovery";
+	public static final String ENDPOINT_PATH_SINGLE_ENDPOINT_SCRAPING = "/metrics";
+	public static final String ENDPOINT_PATH_SINGLE_TARGET_SCRAPING = "/singleTargetMetrics";
+	public static final String ENDPOINT_PATH_PROMREGATOR_METRICS = "/promregatorMetrics";
+	public static final String ENDPOINT_PATH_CACHE_INVALIDATION = "/cache/invalidate";
 	
 	String HTTP_HEADER_PROMREGATOR_INSTANCE_IDENTIFIER = "X-Promregator-Instance";
 }
