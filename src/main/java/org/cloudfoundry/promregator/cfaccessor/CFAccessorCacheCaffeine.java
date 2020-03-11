6561bb12917796d9e163bd09bde4185ed06a865e
Nico Schmoigl <github@schmoigl-online.de>
2020-03-11 21:22:23 +0100

    SonarCloud: Removal of invalid @NotNull annotation

--- src/main/java/org/cloudfoundry/promregator/cfaccessor/CFAccessorCacheCaffeine.java
+++ src/main/java/org/cloudfoundry/promregator/cfaccessor/CFAccessorCacheCaffeine.java
@@ -30,12 +30,12 @@ import reactor.core.scheduler.Schedulers;
 public class CFAccessorCacheCaffeine implements CFAccessorCache {
 	private static final Logger log = LoggerFactory.getLogger(CFAccessorCacheCaffeine.class);
 
-	private @NonNull AsyncLoadingCache<String, ListOrganizationsResponse> orgCache;
-	private @NonNull AsyncLoadingCache<String, ListOrganizationsResponse> allOrgIdCache;
-	private @NonNull AsyncLoadingCache<CacheKeySpace, ListSpacesResponse> spaceCache;
-	private @NonNull AsyncLoadingCache<String, ListSpacesResponse> spaceIdInOrgCache;
-	private @NonNull AsyncLoadingCache<CacheKeyAppsInSpace, ListApplicationsResponse> appsInSpaceCache;
-	private @NonNull AsyncLoadingCache<String, GetSpaceSummaryResponse> spaceSummaryCache;
+	private AsyncLoadingCache<String, ListOrganizationsResponse> orgCache;
+	private AsyncLoadingCache<String, ListOrganizationsResponse> allOrgIdCache;
+	private AsyncLoadingCache<CacheKeySpace, ListSpacesResponse> spaceCache;
+	private AsyncLoadingCache<String, ListSpacesResponse> spaceIdInOrgCache;
+	private AsyncLoadingCache<CacheKeyAppsInSpace, ListApplicationsResponse> appsInSpaceCache;
+	private AsyncLoadingCache<String, GetSpaceSummaryResponse> spaceSummaryCache;
 	
 	@Value("${cf.cache.timeout.org:3600}")
 	private int refreshCacheOrgLevelInSeconds;
