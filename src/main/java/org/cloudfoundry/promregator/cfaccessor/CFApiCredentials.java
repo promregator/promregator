5e7afe2f24e2a8cc7cc29484a7230767705e5d56
Nico Schmoigl <github@schmoigl-online.de>
2021-06-26 13:50:30 +0200

    Enhancing documentation about extending Promregator

--- src/main/java/org/cloudfoundry/promregator/cfaccessor/CFApiCredentials.java
+++ src/main/java/org/cloudfoundry/promregator/cfaccessor/CFApiCredentials.java
@@ -6,7 +6,7 @@ import org.cloudfoundry.promregator.meta.Released;
  * Interface to derive username and password used for accessing the CF API 
  * programmatically.
  * 
- * An extension may implement this interface to provide username and password
+ * A Promregator extension may implement this interface to provide username and password
  * each time Promregator requires the credentials for authenticating towards
  * the associated Cloud Foundry API server.
  * The derivation may happen dynamically (i.e. the credentials may change over
@@ -14,6 +14,14 @@ import org.cloudfoundry.promregator.meta.Released;
  * It is considered an implementation detail of the extension how the 
  * credentials are being determined, e.g. read from a vault or generated
  * based on a certain schema / private key.
+ * 
+ * To make Promregator use your implementation do the following:
+ * <ul>
+ * <li>Ensure that neither the configuration parameters <code>cf.username</code>
+ * nor <code>cf.password</code> are set.</li>
+ * <li>Register a new Spring Bean which returns an instance of type
+ * CFApiCredentials</li>
+ * </ul>
  */
 @Released(since="0.10.0")
 public interface CFApiCredentials {
