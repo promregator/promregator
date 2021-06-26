cd9a065ad1bfe6aa11463353c34710d935b73c57
Nico Schmoigl <github@schmoigl-online.de>
2021-06-26 13:30:24 +0200

    Introduction of Annotation @Released to create public solution model

--- src/main/java/org/cloudfoundry/promregator/cfaccessor/CFApiCredentials.java
+++ src/main/java/org/cloudfoundry/promregator/cfaccessor/CFApiCredentials.java
@@ -1,5 +1,8 @@
 package org.cloudfoundry.promregator.cfaccessor;
 
+import org.cloudfoundry.promregator.meta.Released;
+
+@Released(since="0.10.0")
 public interface CFApiCredentials {
 	String getUsername();
 	String getPassword();
