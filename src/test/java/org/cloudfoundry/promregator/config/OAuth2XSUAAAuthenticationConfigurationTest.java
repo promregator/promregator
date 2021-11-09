1114b8232d97d7805b741ca2b1f8b41d6701ec9e
Marcus Holl <marcus.holl@sap.com>
2021-11-09 11:16:41 +0100

    Remove outdated comment in test

--- src/test/java/org/cloudfoundry/promregator/config/OAuth2XSUAAAuthenticationConfigurationTest.java
+++ src/test/java/org/cloudfoundry/promregator/config/OAuth2XSUAAAuthenticationConfigurationTest.java
@@ -10,13 +10,6 @@ public class OAuth2XSUAAAuthenticationConfigurationTest {
 
 	@Test
 	public void testTokenServiceURLBackwardCompatibility() {
-
-		/*
-		 * We can still receive an old url containing the path via 'tokenServiceUrl'.
-		 * But the corresponding new property 'url' does not contain the path. The url
-		 * is expected without the path by token-client.
-		 */
-
 		OAuth2XSUAAAuthenticationConfiguration subject = new OAuth2XSUAAAuthenticationConfiguration();
 		subject.setTokenServiceURL("https://example.org/oauth/token");
 		assertThat(subject.getTokenServiceURL(), equalTo("https://example.org/oauth/token"));
