9eafb3736e2c8d35d74758dca16c792a6accc7fb
Marcus Holl <marcus.holl@sap.com>
2021-11-15 08:22:45 +0100

    Add missing 'abstract'

--- src/main/java/org/cloudfoundry/promregator/config/AbstractOAuth2XSUAAAuthenticationConfiguration.java
+++ src/main/java/org/cloudfoundry/promregator/config/AbstractOAuth2XSUAAAuthenticationConfiguration.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
-public class AbstractOAuth2XSUAAAuthenticationConfiguration {
+public abstract class AbstractOAuth2XSUAAAuthenticationConfiguration {
 
 	protected String client_id;
 
