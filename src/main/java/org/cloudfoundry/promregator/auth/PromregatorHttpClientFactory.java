02abc655dbf22a3e9a82bbf82052b5aa3d31794f
Marcus Holl <marcus.holl@sap.com>
2021-11-16 09:57:11 +0100

    Use our own http client factory
    At the we are returning a http client where only the ssl context is
    configured.
    
    With that we are getting ride of a warning issues by
    the default impl 'In productive environment provide well configured
    HttpClientFactory service'.

---
+++ src/main/java/org/cloudfoundry/promregator/auth/PromregatorHttpClientFactory.java
@@ -0,0 +1,46 @@
+package org.cloudfoundry.promregator.auth;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+
+import javax.net.ssl.SSLContext;
+
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.sap.cloud.security.client.HttpClientException;
+import com.sap.cloud.security.config.ClientIdentity;
+import com.sap.cloud.security.mtls.SSLContextFactory;
+
+public class PromregatorHttpClientFactory implements com.sap.cloud.security.client.HttpClientFactory {
+
+	private static final Logger log = LoggerFactory.getLogger(PromregatorHttpClientFactory.class);
+
+	@Override
+	public CloseableHttpClient createClient(ClientIdentity clientIdentity) throws HttpClientException {
+
+			log.info("Using '{}' http client factory.", getClass().getSimpleName());
+			if (clientIdentity != null && clientIdentity.isCertificateBased()) {
+				log.debug("Setting up HTTPS client with: certificate: {}\n", clientIdentity.getCertificate());
+				SSLContext sslContext;
+				try {
+					sslContext = SSLContextFactory.getInstance().create(clientIdentity);
+				} catch (IOException | GeneralSecurityException e) {
+					throw new HttpClientException(
+							String.format("Couldn't set up https client for service provider. %s.",
+									e.getMessage()));
+				}
+				SSLConnectionSocketFactory socketFactory = new SSLConnectionSocketFactory(sslContext);
+				return HttpClients.custom()
+						.setSSLContext(sslContext)
+						.setSSLSocketFactory(socketFactory)
+						.build();
+			}
+			log.debug("Setting up default http client");
+			return HttpClients.createDefault();
+	}
+
+}
