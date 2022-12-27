20f2f15a9d9e37dc8616f7ce7ae98db6d25cf19a
Nico Schmoigl <github@schmoigl-online.de>
2022-12-27 15:52:04 +0100

    With Single Endpoint Scraping gone, #104 cannot happen anymore

--- src/test/java/org/cloudfoundry/promregator/rewrite/MergableMetricFamilySamplesTest.java
+++ src/test/java/org/cloudfoundry/promregator/rewrite/MergableMetricFamilySamplesTest.java
@@ -1,9 +1,5 @@
 package org.cloudfoundry.promregator.rewrite;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.LinkedList;
@@ -11,12 +7,10 @@ import java.util.List;
 import java.util.Vector;
 
 import org.cloudfoundry.promregator.JUnitTestUtils;
-import org.cloudfoundry.promregator.textformat004.Parser;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import io.prometheus.client.Collector;
 import io.prometheus.client.Collector.MetricFamilySamples;
 import io.prometheus.client.Collector.MetricFamilySamples.Sample;
 import io.prometheus.client.Collector.Type;
@@ -103,32 +97,5 @@ class MergableMetricFamilySamplesTest {
 		
 		Assertions.assertFalse(returnedEMFS.hasMoreElements());
 	}
-
-	@Test
-	void testIssue104() throws IOException, URISyntaxException {
-		String textToParse = new String(Files.readAllBytes(Paths.get(getClass().getResource("issue104-instance0.text004").toURI())));
-		
-		Parser source0 = new Parser(textToParse);
-		HashMap<String, Collector.MetricFamilySamples> map0 = source0.parse();
-		Assertions.assertEquals(50, map0.size());
-		
-		
-		textToParse = new String(Files.readAllBytes(Paths.get(getClass().getResource("issue104-instance1.text004").toURI())));
-		
-		Parser source1 = new Parser(textToParse);
-		HashMap<String, Collector.MetricFamilySamples> map1 = source1.parse();
-		Assertions.assertEquals(50, map1.size());
-		
-		MergableMetricFamilySamples subject = new MergableMetricFamilySamples();
-		
-		subject.merge(map0);
-		subject.merge(map1);
-		
-		String result = subject.toType004String();
-		
-		String expectedResult = new String(Files.readAllBytes(Paths.get(getClass().getResource("issue104-result.text004").toURI())));
-		Assertions.assertEquals(expectedResult, result);
-	}
-
 
 }
