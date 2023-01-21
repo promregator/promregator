40ddd9430ced2a2ec13a81852234f24f1739cc2d
Nico Schmoigl <github@schmoigl-online.de>
2023-01-21 21:05:41 +0100

    Simplification: Make use of Java records for CacheKeys

--- src/main/java/org/cloudfoundry/promregator/cfaccessor/CacheKeySpace.java
+++ src/main/java/org/cloudfoundry/promregator/cfaccessor/CacheKeySpace.java
@@ -1,15 +1,6 @@
 package org.cloudfoundry.promregator.cfaccessor;
 
-public class CacheKeySpace {
-
-	private String orgId;
-	private String spaceName;
-	
-	public CacheKeySpace(String orgId, String spaceName) {
-		super();
-		this.orgId = orgId;
-		this.spaceName = spaceName;
-	}
+public record CacheKeySpace(String orgId, String spaceName) {
 
 	/**
 	 * @return the orgId
@@ -25,53 +16,4 @@ public class CacheKeySpace {
 		return spaceName;
 	}
 
-	/* (non-Javadoc)
-	 * Auto-generated using Eclipse
-	 * @see java.lang.Object#hashCode()
-	 */
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((orgId == null) ? 0 : orgId.hashCode());
-		result = prime * result + ((spaceName == null) ? 0 : spaceName.hashCode());
-		return result;
-	}
-
-	/* (non-Javadoc)
-	 * Auto-generated using Eclipse
-	 * @see java.lang.Object#equals(java.lang.Object)
-	 */
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		CacheKeySpace other = (CacheKeySpace) obj;
-		if (orgId == null) {
-			if (other.orgId != null)
-				return false;
-		} else if (!orgId.equals(other.orgId)) {
-			return false;
-		}
-		if (spaceName == null) {
-			if (other.spaceName != null)
-				return false;
-		} else if (!spaceName.equals(other.spaceName)) {
-			return false;
-		}
-		return true;
-	}
-
-	/* (non-Javadoc)
-	 * @see java.lang.Object#toString()
-	 */
-	@Override
-	public String toString() {
-		return "CacheKeySpace [orgId=" + orgId + ", spaceName=" + spaceName + "]";
-	}
-	
 }
