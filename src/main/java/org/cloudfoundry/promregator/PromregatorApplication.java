1d3bdb8d89e2f2bf936fb27b238e8d0a29beb737
Nico Schmoigl <github@schmoigl-online.de>
2023-03-18 11:01:52 +0100

    Removing unused imports

--- src/main/java/org/cloudfoundry/promregator/PromregatorApplication.java
+++ src/main/java/org/cloudfoundry/promregator/PromregatorApplication.java
@@ -42,7 +42,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
-import io.prometheus.client.CollectorRegistry;
 import io.prometheus.client.hotspot.DefaultExports;
 import reactor.core.publisher.Hooks;
 
