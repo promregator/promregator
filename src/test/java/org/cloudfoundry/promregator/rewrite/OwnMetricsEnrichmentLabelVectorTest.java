078d52118550858eb9803cb3bc597f0b5452e8de
Nico Schmoigl <github@schmoigl-online.de>
2023-04-16 21:16:19 +0200

    CodeQL: unused class in unit test

--- src/test/java/org/cloudfoundry/promregator/rewrite/OwnMetricsEnrichmentLabelVectorTest.java
+++ src/test/java/org/cloudfoundry/promregator/rewrite/OwnMetricsEnrichmentLabelVectorTest.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-class OwnMetricsEnrichmentLabelVectorTest {
+public class OwnMetricsEnrichmentLabelVectorTest {
 
 	@Test
 	void testWithLabelRewrite() {
